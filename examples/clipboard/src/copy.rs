5099948132314d61b614440f8210ad91a7d9e2a3
proxin187 <thugshaker627@gmail.com>
2024-10-29 21:57:22 +0100

    fixed the examples

--- examples/clipboard/src/copy.rs
+++ examples/clipboard/src/copy.rs
@@ -2,14 +2,9 @@ use yaxi::display;
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut display = display::open_unix(1)?;
+    let mut display = display::open_unix(0)?;
 
     let mut clipboard = display.clipboard()?;
-
-    // TODO: clipboard doesnt work yet.
-    //
-    // thread '<unnamed>' panicked at /home/proxin/Rust/xrs/src/proto/mod.rs:330:18:
-    // internal error: entered unreachable code
 
     clipboard.set_text("test 123")?;
 
